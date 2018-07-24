23d11030f72cbe0395f8972f38ccef0bb3e6b147
Daria <medvedeva.ds@gmail.com>
2018-07-24 18:37:02 +0500

    Create analize_hybridization_parameters.py
    Rewrite hybridization (lambda) parameters from file fort.17

---
+++ analize_hybridization_parameters.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+f = open("fort.17", "r")
+f_lambda = open("lambda_for_iterations.dat", "w")
+lines = f.readlines()
+count = 0
+array = []
+for line in lines:
+    if count < 3:
+        array.append(float(line.split()[0]))
+    if count == 2:
+        f_lambda.write(str(array[0]))
+    	f_lambda.write(' ')
+	f_lambda.write(str(array[1]))
+	f_lambda.write(' ')
+	f_lambda.write(str(array[2]))
+	f_lambda.write('\n')
+    count += 1
+
+    if count >= 6:
+        array[:] = []
+        count = 0
+    else:
+        continue
+
+f.close()
+f_lambda.close()
